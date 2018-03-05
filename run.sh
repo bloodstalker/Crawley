2f26c9a3797dee389fc7dc572990013b8e8303fe
bloodstalker <thabogre@gmail.com>
2018-03-06 01:33:48 +0330

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 cd $(dirname $0)
-./crawleyd --url http://www.tsetmc.com/Loader.aspx\?ParTree\=151311\&i\=67059303301834130
+./crawleyd --demon --slumber 10 --url http://www.tsetmc.com/Loader.aspx\?ParTree\=151311\&i\=67059303301834130 &
 
