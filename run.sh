4be85da3ced95af8a90511d2e02bfc0df8ba479f
bloodstalker <thabogre@gmail.com>
2018-03-06 00:54:23 +0330

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 cd $(dirname $0)
-./main.py --url http://www.tsetmc.com/Loader.aspx\?ParTree\=151311\&i\=67059303301834130
+./crawleyd --url http://www.tsetmc.com/Loader.aspx\?ParTree\=151311\&i\=67059303301834130
 
